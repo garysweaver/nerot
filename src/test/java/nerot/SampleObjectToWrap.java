947ae6417b5678b808e23087e530c736a499e748
Gary S. Weaver <garysweaver@gmail.com>
2010-10-08 11:45:23 -0400

    IDea cleanup of imports, etc.

--- src/test/java/nerot/SampleObjectToWrap.java
+++ src/test/java/nerot/SampleObjectToWrap.java
@@ -3,20 +3,20 @@ package nerot;
 import java.util.Date;
 
 public class SampleObjectToWrap {
-    
+
     public String someFantasticMethod() {
         throw new RuntimeException("called someFantasticMethod() by accident");
     }
-    
-    public String someFantasticMethod( String s, Date date, boolean b, char c, String d, Character ch) {
+
+    public String someFantasticMethod(String s, Date date, boolean b, char c, String d, Character ch) {
         throw new RuntimeException("called someFantasticMethod( String s, Date date, boolean b, char c, String d, Character ch) by accident");
     }
-    
-    public String someFantasticMethod( String s, Date date, boolean b, char c) {
+
+    public String someFantasticMethod(String s, Date date, boolean b, char c) {
         throw new RuntimeException("called someFantasticMethod( String s, Date date, boolean b, char c) by accident");
     }
-    
-    public String someFantasticMethod( String s, Date date, boolean b, char c, double d, Character ch) {
+
+    public String someFantasticMethod(String s, Date date, boolean b, char c, double d, Character ch) {
         return "" + s + "," + date + "," + b + "," + c + "," + d + "," + ch;
     }
 }
--- src/test/java/nerot/SystemTest.java
+++ src/test/java/nerot/SystemTest.java
@@ -1,38 +1,33 @@
 package nerot;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.sun.syndication.feed.synd.SyndContent;
-import com.sun.syndication.feed.synd.SyndContentImpl;
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
-import com.sun.syndication.io.SyndFeedInput;
+
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import static org.junit.Assert.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"../nerot.xml"})
+@ContextConfiguration(locations = {"../nerot.xml"})
 public class SystemTest extends AbstractDependencyInjectionSpringContextTests {
 
     private Nerot nerot;
-    
+
     @After
     public void cleanUpJobs() throws Throwable {
         nerot.unscheduleAll();
     }
-    
+
     @Autowired
     public void setNerot(Nerot nerot) {
         this.nerot = nerot;
@@ -42,24 +37,26 @@ public class SystemTest extends AbstractDependencyInjectionSpringContextTests {
         return nerot;
     }
 
-    @Test public void testRss() throws Throwable {
+    @Test
+    public void testRss() throws Throwable {
         String url = "http://news.google.com/news?ned=us&topic=t&output=rss";
-        
+
         // Schedule job.
         // Syntax at http://www.quartz-scheduler.org/docs/tutorials/crontrigger.html
         nerot.scheduleRss(url, "0/1 * * * * ?");
-        
+
         // Ordinarily you wouldn't wait and would just call the get on a different thread whenever you wanted, but here we wait like any normal async method that isn't event-driven.
         waitForNerot();
-        
+
         // Validate it ran task and stored result.
         // If feed is slow or down, this may fail.
         SyndFeed feed = nerot.getRss(url);
         assertNotNull("Feed was null", feed);
         print(feed);
     }
-    
-    @Test public void testStaticMethod() throws Throwable {
+
+    @Test
+    public void testStaticMethod() throws Throwable {
         String key = "foo";
 
         // Wrap Math.random().
@@ -67,43 +64,44 @@ public class SystemTest extends AbstractDependencyInjectionSpringContextTests {
         task.setKey(key);
         task.setActor(Math.class);
         task.setMethod("random");
-        
+
         // Schedule job.
         // syntax: http://www.quartz-scheduler.org/docs/tutorials/crontrigger.html
         nerot.schedule("myJob", task, "0/1 * * * * ?");
-        
+
         // Ordinarily you wouldn't wait and would just call the get on a different thread whenever you wanted, but here we wait like any normal async method that isn't event-driven.
         waitForNerot();
-        
+
         // Validate it ran task and stored result.
         Object result = nerot.get(key);
         assertNotNull("Result was null", result);
         System.err.println(result);
     }
-    
-    @Test public void testInstanceMethod() throws Throwable {
-        String key = "foo";        
+
+    @Test
+    public void testInstanceMethod() throws Throwable {
+        String key = "foo";
 
         // Wrap a sample object with GenericTask
         GenericTask task = new GenericTask();
         task.setKey(key);
         task.setActor(new SampleObjectToWrap());
         task.setMethod("someFantasticMethod");
-        task.setArgs(new Object[] {"s", new Date(), true, 'c', 1.0D, new Character('c')});
-        
+        task.setArgs(new Object[]{"s", new Date(), true, 'c', 1.0D, new Character('c')});
+
         // Schedule job.
         // Syntax at http://www.quartz-scheduler.org/docs/tutorials/crontrigger.html
         nerot.schedule("myJob", task, "0/1 * * * * ?");
-        
+
         // Ordinarily you wouldn't wait and would just call the get on a different thread whenever you wanted, but here we wait like any normal async method that isn't event-driven.
         waitForNerot();
-        
+
         // Validate it ran task and stored result.
         Object result = nerot.get(key);
         assertNotNull("Result was null", result);
         System.err.println(result);
     }
-    
+
     private void waitForNerot() {
         try {
             // Longer than I needed, but may need to set this higher if your machine is really slow. Wait on Quartz thread to spin up, read schedule, and perform task (usually < 3 sec).
