17498d672cd096c188f716155d454627cfe853a3
Gary S. Weaver <garysweaver@gmail.com>
2010-10-22 14:59:01 -0400

    updated portlet example code and added config

--- examples/PortletController.java
+++ examples/PortletController.java
@@ -1,8 +1,12 @@
-package com.acme;
+package nerot.example;
 
 import com.sun.syndication.feed.synd.SyndFeed;
 import nerot.Nerot;
-import org.springframework.beans.factory.InitializingBean;
+import nerot.store.Storer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.portlet.ModelAndView;
 import org.springframework.web.portlet.mvc.AbstractController;
 
@@ -13,50 +17,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PortletController extends AbstractController implements InitializingBean {
+public class PortletController extends AbstractController {
+    private static final Logger LOG = LoggerFactory.getLogger(UgradEdNewsController.class);
 
+    @Autowired
     private Nerot nerot;
-    private String feedUrl = null;
-    private String feedSchedule = null;
-    
-    public Nerot getNerot() {
-        return nerot;
-    }
-    
-    public void setNerot(Nerot nerot) {
-        this.nerot = nerot;
-    }
-    
-    public String getFeedSchedule() {
-        return feedSchedule;
-    }
-    
-    public void setFeedSchedule(String feedSchedule) {
-        this.feedSchedule = feedSchedule;
-    }
-    
-    public String getFeedUrl() {
-        return feedUrl;
-    }
-    
-    public void setFeedUrl(String feedUrl) {
-        this.feedUrl = feedUrl;
-    }
-    
-    public void afterPropertiesSet() throws Exception {
-    
-        // This schedules the feed immediately after the bean is instantiated and it should run at least once to start
-        nerot.scheduleRss(feedUrl, feedSchedule);
-    }
-    
+
+    @Autowired
+    @Qualifier("rssScheduler")
+    private Storer storer;
+
     protected ModelAndView handleRenderRequestInternal(RenderRequest renderRequest, RenderResponse renderResponse)
             throws Exception {
-    
+        String feedUrl = storer.getStoreKey();
         Map<String, Object> params = new HashMap<String, Object>();
-        
-        // This gets the feed from the store
         SyndFeed feed = nerot.getRssFromStore(feedUrl);
-        
+
         if (feed==null) {
             logger.warn("Got null feed from Nerot for '" + feedUrl + "'");
             params.put("entries", new ArrayList());
